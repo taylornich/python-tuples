29a122a760f3a259f5f338a6f0bb20425c333a85
taylornich <taylormanhard15@gmail.com>
2024-07-14 23:45:34 -0700

    Question 2

---
+++ data_structure_challenges_real_world.py
@@ -0,0 +1,14 @@
+# question 1 task 1
+
+library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
+
+def add_to_library(title, author):
+    new_book = (title, author)
+
+    if new_book in library:
+        print("Duplicate Book")
+    else:
+        library.append(new_book)
+
+book = add_to_library("1984", "George Orwell")
+print(library)
