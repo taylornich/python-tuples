bc11c5ea450b2d9b9c91c8a919b83faec7d7421d
taylornich <taylormanhard15@gmail.com>
2024-07-14 23:12:09 -0700

    Question 1

---
+++ tuple_mastery.py
@@ -0,0 +1,11 @@
+# question 1 task 1
+
+itineraries = (("Alice", "New York", "London"), ("Bob", "Tokyo", "San Francisco"))
+
+for itinerary in itineraries:
+    name, departure_location, destination = itinerary
+    index = 0
+    print(f"Itinerary {index + 1}: {name} - From {departure_location} to {destination}.")
+
+
+    
